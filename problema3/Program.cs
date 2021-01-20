63d3e666d5137b8ea06575a34ac6010e63d16ebf
LarisaOprea <72555984+LarisaOprea@users.noreply.github.com>
2021-01-20 17:47:10 +0200

    Add files via upload

---
+++ problema3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///3.Sa se determine pozitiile dintr-un vector 
+            ///pe care apar cel mai mic si cel mai mare element al vectorului.
+            int n, maxim, minim, pozmaxim = 0, pozminim = 0;
+            n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            v[0] = int.Parse(Console.ReadLine());
+            maxim = v[0];
+            minim = v[0];
+            for (int i = 1; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+                if (v[i] > maxim)
+                {
+                    maxim = v[i];
+                    pozmaxim = i;
+                }
+                else if (v[i] < minim)
+                {
+                    minim = v[i];
+                    pozminim = i;
+                }
+            }
+            Console.WriteLine("Maximul este " + maxim + " pe pozitia " + pozmaxim + ", iar minimul este " + minim + " pe pozitia " + pozminim);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ problema4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///4.Deteminati printr-o singura parcurgere, 
+            ///cea mai mica si cea mai mare valoare dintr-un vector si de cate ori apar acestea.
+            int n, maxim, nrmaxim = 0, minim, nrminim;
+            n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            v[0] = int.Parse(Console.ReadLine());
+            maxim = v[0];
+            minim = v[0];
+            nrmaxim = 1;
+            nrminim = 1;
+            for (int i = 1; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+                if (v[i] > maxim)
+                {
+                    maxim = v[i];
+                    nrmaxim = 1;
+                }
+                else if (v[i] == maxim) nrmaxim++;
+
+                if (v[i] < minim)
+                {
+                    minim = v[i];
+                    nrminim = 1;
+                }
+                else if (v[i] == minim) nrminim++;
+            }
+            Console.WriteLine("Maximul este " + maxim + " si apare de " + nrmaxim + " ori.");
+            Console.WriteLine("Minimul este " + minim + " si apare de " + nrminim + " ori.");
+
+            Console.ReadKey();
+;        }
+    }
+}
